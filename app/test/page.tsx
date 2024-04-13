6530114adb38d560d4f241a9d7858aaf3c8ee172
ChedlyRebai <chedly.rebai123@gmail.com>
2024-04-13 23:49:27 +0100

    Translate text from English to Greek in test page

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,17 @@
+import { pipeline } from '@xenova/transformers';
+
 export default async function page() {
+    // Allocation a pipeline for translation
+let translator:any = await pipeline('translation', 'Xenova/nllb-200-distilled-600M');
+
+// Translate from English to Greek
+let result = await translator('I like to walk my dog.', {
+    src_lang: 'eng_Latn',
+    tgt_lang: 'ell_Grek'
+});
     return (
         <div>
-            <h1>Test Page</h1>
+            <h1>{result}</h1>
             <p>This is a test page</p>
         </div>
     )
